2bb01710ccd52a498b2e1e7bcfd2ddca0a6f1839
Mark Koh <mkoh@rjmetrics.com>
2015-10-16 03:28:10 +0000

    Regenerated with JS instead of coffee.

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('jamjar2')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
