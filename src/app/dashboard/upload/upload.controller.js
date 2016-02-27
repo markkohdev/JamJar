0b2afbbb1f65ffead0bf0e83b74a0c711bc81272
Drew Banin <dsb59@drexel.edu>
2016-02-27 20:54:40 +0000

    artist search on update page (hits API)

--- src/app/dashboard/upload/upload.controller.js
+++ src/app/dashboard/upload/upload.controller.js
@@ -4,7 +4,7 @@
       .module('jamjar')
       .controller('UploadController', UploadController);
 
-  function UploadController ($timeout, $q) {
+  function UploadController (ArtistService, $q) {
     var self = this;
 
     var showUpload = false;
@@ -17,16 +17,20 @@
         return showUpload;
     }
       
-    self.readonly = false;
     self.selectedItem = null;
     self.searchText = null;
-    self.querySearch = querySearch;
-    self.artists = loadArtists();
+    ///self.querySearch = querySearch;
     self.selectedArtist = [];
     self.autocompleteRequireMatch = false;
-      
+
+    // populated by responses from the API
+    self.searchResults = [];
     self.transformChip = transformChip;
-      
+
+    self.querySearch = function(query) {
+      return ArtistService.search(query);
+    }
+
     /* Return the proper object when the append is called*/
     function transformChip(chip) {
       // If it is an object, it's already a known chip
@@ -37,47 +41,6 @@
       else{
           return { name: chip }
       }
-    }
-      
-    /*Search for artists*/
-    function querySearch (query) {
-      var results = query ? self.artists.filter(createFilterFor(query)) : [];
-      return results;
-    }
-
-    /*Create filter function for a query string*/
-    function createFilterFor(query) {
-      var lowercaseQuery = angular.lowercase(query);
-
-      return function filterFn(artist) {
-        return (artist._lowername.indexOf(lowercaseQuery) === 0);
-      };
-
-    }
-
-    function loadArtists() {
-      var groups = [
-        {
-          'name': 'group1'
-        },
-        {
-          'name': 'group2'
-        },
-        {
-          'name': 'group3'
-        },
-        {
-          'name': 'group4'
-        },
-        {
-          'name': 'group5'
-        }
-      ];
-
-      return groups.map(function (g) {
-        g._lowername = g.name.toLowerCase();
-        return g;
-      });
     }
   }
 })();
