8de18835eb2a5ab629fdede1db3f44d2d149722c
Drew Banin <dsb59@drexel.edu>
2016-02-29 02:38:27 +0000

    functionally working....

--- src/app/components/jamjarPlayer/jamjarPlayer.directive.js
+++ src/app/components/jamjarPlayer/jamjarPlayer.directive.js
@@ -24,8 +24,9 @@
       vm.concert = {};
       vm.playable = {};
       vm.keyVideo = null;
+      vm.played = {};
       vm.rows = 1;
-      vm.cols = 2;
+      vm.cols = 1;
 
       function urlAtOffset(src, offset_seconds) {
         if (offset_seconds) {
@@ -50,7 +51,16 @@
       };
 
       vm.getOffset = function(videoId) {
-        return (vm.getKeyVideo().API.currentTime / 1000.0) - vm.getVideoById(videoId).edges[vm.keyVideo].offset;
+        var edges = vm.getVideoById(videoId).edges;
+        var edge = _.find(edges, {video: parseInt(vm.keyVideo)});
+        if (!edge) {
+          debugger
+        }
+        var offset = (vm.getKeyVideo().API.currentTime / 1000.0) + edge.offset;
+
+        console.log("VIDEO", videoId, "OFFSET", offset);
+
+        return offset;
       }
 
       vm.getKeyVideo = function() {
@@ -70,17 +80,21 @@
         };
 
         // add it to the now-playing object -- this will create a videogular player for the video
+        video.loadStart = new Date();
         vm.playable[video.id] = video;
       };
 
       vm.getPlayableVideos = function() {
         var keyVideo = vm.getKeyVideo();
 
+        if (!keyVideo) {
+          debugger
+        }
         var playableEdges = _.filter(keyVideo.edges, function(edge) {
           // require video length to be < requested offset position AND offset > 0
           var video = vm.getVideoById(edge.video);
           var offset = (vm.getKeyVideo().API.currentTime / 1000.0) - edge.offset;
-          return offset > 0 && offset < video.length && edge.confidence >= 5;
+          return offset > 0 && offset < video.length && edge.confidence >= 5 && !vm.played[edge.video];
         });
 
         return _.map(playableEdges, function(edge) {
@@ -89,13 +103,25 @@
       };
 
       vm.onUpdateSource = function(src, videoId) {
-        if (videoId != vm.keyVideo) {
-          var offset = vm.getOffset(videoId);
-          vm.playable[videoId].API.seekTime(offset);
-        }
+        vm.cols = _.keys(vm.playable).length;
       },
 
-      vm.onUpdateTime = function() {
+      vm.onUpdateState = function(state, videoId) {
+        vm.cols = _.keys(vm.playable).length;
+
+        if (state == 'play' && videoId != vm.keyVideo && vm.playable[videoId]) {
+          var offset = vm.getOffset(videoId);
+          var loadingOffset = (new Date() - vm.playable[videoId].loadStart) / 1000.0;
+          //vm.playable[videoId].API.seekTime(offset + loadingOffset);
+        }
+      };
+
+      vm.onUpdateTime = function(time, videoId) {
+        vm.cols = _.keys(vm.playable).length;
+        vm.updatePlayable();
+      }
+
+      vm.updatePlayable = function() {
         var videos = vm.getPlayableVideos();
         _.each(videos, function(video) {
           if (vm.playable[video.id]) {
@@ -108,19 +134,35 @@
         });
       };
 
-      vm.onComplete = function() {
-        //vm.setValidConnections();
-        return;
+      vm.onComplete = function(videoId) {
+        vm.cols = _.keys(vm.playable).length;
+        console.log('on complete', videoId);
+        // remove current key from playable
+        // promote a new video to be the key
+        // pray
+        console.log('playable', vm.playable);
+        if (_.keys(vm.playable).length <= 1) {
+          return;
+          // out of videos -- what do we do now??
+        }
 
-        if (vm.connections.length == 0) return;
+        console.log('videoId', videoId, 'vm.keyVideo', vm.keyVideo);
+        if (videoId == vm.keyVideo) {
+          var potentialKeys = _.without(_.keys(vm.playable), videoId);
+          // ideally this wouldn't be done randomly... function of confidence, upvotes, etc
+          var keyIndex = _.random(0, potentialKeys.length - 1); // inclusive
+          var newKeyVideo = potentialKeys[keyIndex];
 
-        // ideally this would sort by some combination of connected videos, length, confidence, etc
-        var sorted = _.sortBy(vm.connections, function(edge) {
-          var video = vm.getVideoById(edge.video);
-          return -video.length;
-        });
+          vm.keyVideo = newKeyVideo;
+          vm.getKeyVideo().API.setVolume(0.0);
+        }
+        // if key video (or otherwise), delete form playable and update accordingly
+        vm.played[videoId] = 1;
+        delete vm.playable[videoId];
+        console.log("DELETED", videoId, "from playable");
+        console.log(vm.playable);
 
-        vm.setSourceByVideoId(sorted[0].video, sorted[0].offset);
+        vm.updatePlayable();
       };
 
       // fetch the concert graph and add the first video as a source
@@ -140,7 +182,7 @@
         if (vm.playable[parseInt(videoId)]) {
           vm.playable[videoId].API = API;
           if (videoId == vm.keyVideo) {
-            API.setVolume(0.5);
+            API.setVolume(0.0);
           } else {
             API.setVolume(0.0);
           }
