c06d17df72dbdcf5fdcc23af9d6b5ec0dbfbbe16
Drew Banin <dsb59@drexel.edu>
2016-03-13 22:16:15 +0000

    use api artists list for a concert

--- src/app/dashboard/concert/concert.controller.js
+++ src/app/dashboard/concert/concert.controller.js
@@ -11,7 +11,10 @@
         vm.concert_id = $state.params.id;
         vm.concert = {};
         vm.concert_graph = {};
-        vm.concert_artists = "";
+
+        vm.getConcertArtists = function() {
+          return _.map(vm.concert.artists, 'name').join(", ");
+        }
 
         ConcertService.getConcertById(vm.concert_id, function(err, res) {
             if (err) {
@@ -20,14 +23,6 @@
             }
 
             vm.concert = res;
-            
-            _.each(vm.concert.videos[0].artists, function(artist){                
-                vm.concert_artists += artist.name + ", ";
-            });
-            
-            vm.concert_artists = vm.concert_artists.slice(0, -2);
-            
-            console.log(vm.concert);
         });
 
         ConcertService.getGraphById(vm.concert_id, function(err, res) {
