8c55313c68148cc11b668c372ae3a56d164e1d2d
Jess Eng <jessicaeng55@yahoo.com>
2016-03-13 05:54:57 +0000

    fixed some metadata display

--- src/app/dashboard/concert/concert.controller.js
+++ src/app/dashboard/concert/concert.controller.js
@@ -11,24 +11,47 @@
         vm.concert_id = $state.params.id;
         vm.concert = {};
         vm.concert_graph = {};
+        vm.concert_artists = "";
 
         ConcertService.getConcertById(vm.concert_id, function(err, res) {
-          if (err) {
-            debugger
-            return alert('error');
-          }
+            if (err) {
+                debugger
+                return alert('error');
+            }
 
-          vm.concert = res;
+            vm.concert = res;
+            
+            _.each(vm.concert.videos[0].artists, function(artist){                
+                vm.concert_artists += artist.name + ", ";
+            });
+            
+            vm.concert_artists = vm.concert_artists.slice(0, -2);
+            
+            console.log(vm.concert);
         });
 
         ConcertService.getGraphById(vm.concert_id, function(err, res) {
-          if (err) {
-            debugger
-            return alert('error');
-          }
+            if (err) {
+                debugger
+                return alert('error');
+            }
 
-          vm.concert_graph = res;
+            vm.concert_graph = res;
+            
+            _.each(vm.concert_graph.graph, function(jamjar){
+                var video_ids = _.keys(jamjar.adjacencies);
+                
+                jamjar.thumbnails = _.map(video_ids, function(videoId){
+                    var video = _.find(vm.concert_graph.concert.videos, {id:parseInt(videoId)});
+                    if(video){
+                        return video.thumb_src;
+                    }
+                });
+            });
+            
+            console.log(vm.concert_graph);
         });
 
+        
     }
 })();
