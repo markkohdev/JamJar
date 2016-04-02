395bfe737780795db59fdad51c545664a25c3934
Drew Banin <dsb59@drexel.edu>
2016-04-02 20:05:42 +0000

    fix typo and add comment

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,10 +8,13 @@
 
     /** @ngInject */
     
-    function MainController($anchorScroll, $location, $scope, $state, TokenService) {
+    function MainController($anchorScroll, $location, $scope, $state, TokenService, AuthService) {
         var vm = this;
 
-        if (TokenService.getToken() && TokenService.getUser()) {
+        // if a 401 Unauthorized response is returned, then the token and user are 
+        // cleared from local storage. This ensures that the user won't fall into an
+        // infinite loop! If they are already authed, just redirect to discover
+        if (TokenService.getToken() && AuthService.getUser()) {
           return $state.go('dashboard.discover');
         }
 
