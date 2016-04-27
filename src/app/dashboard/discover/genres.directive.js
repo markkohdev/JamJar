ec96c4c1a99a494aaf47efcc561aacf374f36169
Drew Banin <dsb59@drexel.edu>
2016-04-27 18:32:35 +0000

    dynamic genres and styling [wip]

--- src/app/dashboard/discover/genres.directive.js
+++ src/app/dashboard/discover/genres.directive.js
@@ -11,50 +11,27 @@
       restrict: 'E',
       templateUrl: 'app/dashboard/discover/genres.html',
       controller: GenresController,
-      controllerAs: 'genres',
+      controllerAs: 'vm',
       bindToController: true
     };
 
     return directive;
 
     /** @ngInject */
-    function GenresController(AuthService, $state) {
-        var vm = this;
+    function GenresController(GenreService) {
+      var vm = this;
 
-        vm.types = [
-            {
-              state: 'dashboard.discover',
-              name: 'R&#38;B'
-            },
-            {
-              state: 'dashboard.discover',
-              name: 'Pop'
-            },
-            {
-              state: 'dashboard.discover',
-              name: 'Folk'
-            },
-            {
-              state: 'dashboard.discover',
-              name: 'Country'
-            },
-            {
-              state: 'dashboard.discover',
-              name: 'Rock'
-            },
-            {
-              state: 'dashboard.discover',
-              name: 'Blues'
-            },
-                        {
-              state: 'dashboard.discover',
-              name: 'Classical'
-            },
-            {
-              state: 'dashboard.discover',
-              name: 'Alternative'
-            }
-        ];
+      vm.genres = [];
+      vm.colors = ["#FDEB65", "#F5A95F", "#F15F4E", "#9BD096", "#7976B4", "#673A96"];
+
+      GenreService.list(function(err, resp) {
+        if (err) {
+          return console.error(err);
+        }
+
+        vm.genres = resp;
+        console.log(resp);
+      });
     }
   }
 
