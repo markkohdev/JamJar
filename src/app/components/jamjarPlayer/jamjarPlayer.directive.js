5fcb7077c4d02d9ae49ed192562f131c10b6eade
Drew Banin <dsb59@drexel.edu>
2016-05-09 04:55:03 +0000

    implement on, auto, off state triggered by buttons

--- src/app/components/jamjarPlayer/jamjarPlayer.directive.js
+++ src/app/components/jamjarPlayer/jamjarPlayer.directive.js
@@ -28,6 +28,9 @@
             // this will be a factory with DI
             vm.jamjar = new JamJar(ConcertService, VideoService, $sce);
             vm.jamjar.initialize(parseInt($stateParams.concert_id), parseInt($stateParams.video_id), $stateParams.type);
+
+            vm.individual = $stateParams.type == 'individual';
+
             vm.jamjar.onPlay = function(video) {
               video.views += 1;
               VideoService.view(video.id, function(err, resp) {
@@ -41,6 +44,7 @@
               visible: false,
               timeout: null,
               mouse: {pageX: 0, pageY: 0},
+              state: null,
             };
 
             vm.onHoverOut = function(event) {
@@ -53,6 +57,10 @@
             }
 
             vm.onHover = function(event) {
+
+              if (vm.overlay.state != 'auto')
+                return
+
               // make overlay visible
               var timeoutMs = 3000;
 
@@ -142,8 +150,20 @@
                 });
             };
 
-            vm.toggleOverlay = function() {
-              vm.overlay.visible = !vm.overlay.visible;
+            vm.toggleOverlay = function(state) {
+              if (state == 'off') {
+                vm.overlay.visible = false;
+                vm.overlay.state = null;
+              } else if (state == 'on') {
+                vm.overlay.state = null;
+                vm.overlay.visible = true;
+                if (vm.overlay.timeout) {
+                  $timeout.cancel(vm.overlay.timeout);
+                }
+              } else if (state == 'auto') {
+                vm.overlay.visible = true;
+                vm.overlay.state = 'auto';
+              }
             };
 
         }
