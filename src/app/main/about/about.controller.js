660b63bd919c15d282329b78632191bab0bc0556
Jessica Eng <jessicaeng55@yahoo.com>
2016-05-17 04:15:54 +0000

    show regular navbar if signed in

--- src/app/main/about/about.controller.js
+++ src/app/main/about/about.controller.js
@@ -6,7 +6,13 @@
         .controller('AboutController', AboutController);
 
     /** @ngInject */
-    function AboutController($scope, $state) {
+    function AboutController($scope, $state, TokenService, AuthService) {
         var vm = this;
+        
+        vm.showDashboardNav = false;
+        
+        if (TokenService.getToken() && AuthService.getUser()) {
+            vm.showDashboardNav = true;
+        }
     }
 })();
